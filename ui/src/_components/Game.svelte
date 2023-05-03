581958340f21aa5e0ec12fc4393ca27953a602df
Yuval Levi <yuvall9313@gmail.com>
2023-05-03 22:55:00 +0300

    Add game and room redirection

---
+++ ui/src/_components/Game.svelte
@@ -0,0 +1,3 @@
+<PlayersScoreboard/>
+<SongTimer/>
+<GameChat/>
--- ui/src/routes/play/solo/+page.svelte
+++ ui/src/routes/play/solo/+page.svelte
@@ -1,10 +1,18 @@
 <script>
     import CategoryPicker from '../../../_components/CategoryPicker.svelte'
+    import Game from '../../../_components/Game.svelte'
+    import { page } from '$app/stores';
+
+    const roomId = $page.url.searchParams.get('room');
 </script>
 
-
+{#if roomId}
+<h1 class="mx-auto">Start guessing!</h1>
+<Game/>
+{:else}
 <h1 class="mx-auto">Play solo</h1>
-<div class="">
+<div>
     <CategoryPicker/>
 </div>
 <button class="btn btn-primary mx-auto btn-block">Play</button>
+{/if}
