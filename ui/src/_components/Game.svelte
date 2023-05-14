ef16b48697f589c2b75531d7c203deca4823f468
Yuval Levi <yuvall9313@gmail.com>
2023-05-14 22:57:20 +0300

    Change aspect ratio of parts in frame

--- ui/src/_components/Game.svelte
+++ ui/src/_components/Game.svelte
@@ -7,16 +7,16 @@
 </script>
 
 <div class="flex flex-col md:flex-row grow w-full">
-	<div class="md:w-1/3 p-4 md:order-first md:sticky md:top-0">
+	<div class="md:w-3/12 p-4 md:order-first md:sticky md:top-0">
 		<button class="md:hidden" on:click={() => (menuOpen = !menuOpen)}>Toggle Menu</button>
 		<div class={`${menuOpen ? 'block' : 'hidden'} md:block`}>
 			<PlayersScoreboard />
 		</div>
 	</div>
-	<div class="md:w-1/3 p-4 flex-1">
+	<div class="md:w-5/12 p-4 flex-auto h-1/6">
 		<GameChat />
 	</div>
-	<div class="md:w-1/3 p-4 flex-1">
+	<div class="md:w-4/12 p-4 flex-auto h-4/6">
 		<SongTimer />
 	</div>
 </div>
