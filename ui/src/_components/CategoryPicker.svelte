a8b4f678a55b82b0afd91527859d600993f1b96b
Yuval Levi <yuvall9313@gmail.com>
2023-05-14 20:20:39 +0300

    Fix game and category picker according to layout

--- ui/src/_components/CategoryPicker.svelte
+++ ui/src/_components/CategoryPicker.svelte
@@ -1,22 +1,22 @@
 <script>
-    import GenreCard from './GenreCard.svelte'
+	import GenreCard from './GenreCard.svelte';
 </script>
 
 <h2>Pick your category:</h2>
-<div class="grid grid-cols-4 gap-7 -mx-20 my-10 place-self-stretch">
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
-    <GenreCard>Genre</GenreCard>
+<div class="grid grid-cols-4 gap-7 my-10 place-self-stretch">
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
+	<GenreCard>Genre</GenreCard>
 </div>
