1f0d67c54375ad2a19eeb5bcbaeecbd7104df675
Yuval Levi <yuvall9313@gmail.com>
2023-06-06 02:39:45 +0300

    Add input that will send message to api

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,4 +1,22 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    async function handleSubmit(event) {
+        const formData = new FormData(event.currentTarget);
+        const response = await fetch('/api/players', {
+            method: 'POST',
+            body: formData,
+        });
+        if (response.ok) {
+            await goto('/play/solo')
+        }
+    }
+</script>
+
 <h1 class="justify-self-center">Welcome to Ongaku Quiz</h1>
 <div class="grid">
-    <a class="btn btn-primary btn-block" href="/play/solo">Start</a>
+    <form on:submit|preventDefault={handleSubmit}>
+        <input type="text" name="player_name" class="input input-bordered w-full max-w-xs" required />
+        <button class="btn btn-primary btn-block" type="submit">Start</button>
+    </form>
 </div>
